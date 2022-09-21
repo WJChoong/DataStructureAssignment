3e2343467fd15789fbbcff460b791a267d0be4a9
LanceYHQ <108929425+LanceYHQ@users.noreply.github.com>
2022-09-22 00:36:37 +0800

    22/Sept/2022

--- Group11_DSTR_Assignment/Group11_DSTR_Assignment/Prototypee.cpp
+++ Group11_DSTR_Assignment/Group11_DSTR_Assignment/Prototypee.cpp
@@ -45,6 +45,17 @@ struct Ticket {
 }*ticketHead, * ticketCurrent;
 
 
+struct Customer {
+	int CustomerId;
+	string CustomerName;
+	string CustomerIcOrPassport;
+	string CustomerPassword;
+
+	Customer* nextAdd;
+}*customerHead,*customerCurrent;
+
+
+//Draw Opening
 void drawopening() {
 	cout << "      -------------------------------------" << endl;
 	cout << "      -------Ticket Booking System---------" << endl;
@@ -53,7 +64,7 @@ void drawopening() {
 
 }
 
-
+//Draw Map
 void drawmap() {
 
 	cout << "       Titiwangsa     PWTC   Sultan Ismail    Majils Jamek    Plaza Rakyat    Hang Tuah    Pudu     Chan Sow Lin" << endl;
@@ -88,8 +99,134 @@ Station* createStationNode(int StationID, string StationName, string PreviousSta
 }
 
 
+//create a Customer node function
+Customer* createCustomerNode(int customerId, string customerName, string customerIcOrPassport, string customerPassword)
+{
+	Customer* CustomerNode = new Customer;
+	CustomerNode->CustomerId = customerId;
+	CustomerNode->CustomerName = customerName;
+	CustomerNode->CustomerIcOrPassport = customerIcOrPassport;
+	CustomerNode->CustomerPassword = customerPassword;
+
+	CustomerNode->nextAdd = NULL;
+
+	return CustomerNode;
+}
+
+
+//Insert Customer Node to the End of the list
+void insertCustomerNodeToTheEndList(Customer* CustomerNode) 
+{
+	//Situation 1: List still empty
+	if (customerHead == NULL)
+	{
+		customerHead = CustomerNode;
+	}
+
+	//Situation 2: List not empty
+	else
+	{
+		customerCurrent = customerHead;
+		while (customerCurrent->nextAdd != NULL)
+		{
+			customerCurrent = customerCurrent->nextAdd;
+		}
+		customerCurrent->nextAdd = CustomerNode;
+	}
+}
+
+
+//Check and Create Customer Account
+int createCustomerAccount(string customerName, string customerIcOrPassport, string customerPassword)
+{
+	int customerId;
+	if (customerHead == NULL)
+	{
+		customerId = 1;
+	}
+	else
+	{
+		customerCurrent = customerHead;
+		//Check account existed or not
+		if (customerCurrent->CustomerIcOrPassport == customerIcOrPassport)
+		{
+			return 0;
+		}
+		while (customerCurrent->nextAdd != NULL)
+		{
+			customerCurrent = customerCurrent->nextAdd;
+			//Check account existed or not
+			if (customerCurrent->CustomerIcOrPassport == customerIcOrPassport)
+			{
+				return 0;
+			}
+		}
+		//Create a Customer Id
+		customerId = customerCurrent->CustomerId + 1;
+	}
+
+	//If the customer does not exist in the system, the account will be created
+	Customer* customerNode = createCustomerNode(customerId, customerName, customerIcOrPassport, customerPassword);
+	insertCustomerNodeToTheEndList(customerNode);
+	return 1;
+}
+
+
+//Check Customer Account and password
+int checkCustomerAccount(string customerIcOrPassport, string customerPassword)
+{
+	if (customerHead == NULL)
+	{
+		cout << "This system has no customer account" << endl;
+		system("pause");
+		return 0;
+	}
+	else
+	{
+		customerCurrent = customerHead;
+		//check customer account
+		if (customerCurrent->CustomerIcOrPassport == customerIcOrPassport)
+		{
+
+			if (customerCurrent->CustomerPassword == customerPassword)
+			{
+				cout << "Welcome Mr/Mrs " << customerCurrent->CustomerName << endl;
+				return customerCurrent->CustomerId;
+			}
+			else
+			{
+				cout << "Incorrect Password. " << endl;
+				system("pause");
+				return 0;
+			}
+		}
+		while (customerCurrent->nextAdd != NULL)
+		{
+			customerCurrent = customerCurrent->nextAdd;
+
+			//check customer account
+			if (customerCurrent->CustomerIcOrPassport == customerIcOrPassport)
+			{
+				if (customerCurrent->CustomerPassword == customerPassword)
+				{
+					cout << "Welcome Mr/Mrs " << customerCurrent->CustomerName << endl;
+					return customerCurrent->CustomerId;
+				}
+				else
+				{
+					cout << "Incorrect Password. " << endl;
+					return 0;
+				}
+			}
+		}
+
+		cout << "This IC or Passport Number does not exist in this system, Please create a new account." << endl;
+		return 0;
+	}
+}
+
 //Insert to the Front of the list
-void insertToTheFrontList(Station* StationNode)
+void insertStationNodeToTheFrontList(Station* StationNode)
 {
 	// If the list still empty
 	if (stationHead == NULL && stationTail == NULL)
@@ -243,96 +380,75 @@ string listAllPossibleDepartureTime(int route, int departure, int todaydate)
 	}
 	
 
-	int size = 38;
-	string* TrainArrivalTime;
-	if (todaydate == 0)
-	{
-		//Create a empty dynamic array to contain all possible departure time
-		TrainArrivalTime = new string[size];
+	int size = 38; // use for input checking later
 
-		//Calculate all possible departure time
-		int hour = 6;
-		int count = 0;
-		while (count != 38) {
-			if (hour == 24)
-			{
-				hour = 0;
-			}
-			cout << count + 1 << ") " << setw(2) << setfill('0') << to_string(hour) << ":" << setw(2) << setfill('0') << to_string(minutes) << endl;
-			TrainArrivalTime[count] = to_string(hour) + ":" + to_string(minutes);
-			count++;
-			minutes = minutes + 30;
-			if (minutes > 59)
-			{
-				minutes = minutes - 60;
-				hour = hour + 1;
-			}
+	//Create a empty dynamic array to contain all possible departure time
+	string* TrainArrivalTime = new string [38];
+
+	//Calculate all possible departure time
+	int hour = 6;
+	int arrayindex1 = 0;
+	while (arrayindex1 != 38) {
+		if (hour == 24)
+		{
+			hour = 0;
 		}
-
+		if (todaydate == 0)
+		{
+			cout << arrayindex1 + 1 << ") " << setw(2) << setfill('0') << to_string(hour) << ":" << setw(2) << setfill('0') << to_string(minutes) << endl;
+		}
+		TrainArrivalTime[arrayindex1] = to_string(hour) + ":" + to_string(minutes);
+		arrayindex1++;
+		minutes = minutes + 30;
+		if (minutes > 59)
+		{
+			minutes = minutes - 60;
+			hour = hour + 1;
+		}
 	}
-	else //VERY BUGGGGGGY NEED MORE TESTING
-	{	
-		//Get Current Date and Time
+
+	int check_hours;
+	int check_minutes;
+	int arrayindex2 = 0;
+	string* TodayTrainArrivalTime = new string[38];
+
+	if (todaydate == 1)
+	{
 		struct tm newtime;
 		time_t now = time(0);
 		localtime_s(&newtime, &now);
 		int CurrentHours = newtime.tm_hour;
 		int CurrentMins = newtime.tm_min;
 
-		//possible hour
-		int hour = CurrentHours;
-
-		// Calculating the size of the dynamic array needed
-		if (hour < 6) // if passenger book on midnight of the current date
+		for (int i = 0; i < 38; i++)
 		{
-			hour = 6;
-			size = 38;
-		}
-		else
-		{
-			size = size - ((hour - 6) * 2);
-			if (CurrentMins >= minutes)
-			{
-				size = size - 1;
-				minutes = minutes + 30;
-				if (minutes > 59)
-				{
-					minutes = minutes - 60;
-					hour = hour + 1;
-				}
-				else if (CurrentMins >= minutes)
-				{
-					size = size - 1;
-					minutes = minutes - 30;
-					hour = hour + 1;
-				}
-			}
-		}
-		
+			int pos = TrainArrivalTime[i].find(":");
 
-		//Create a empty dynamic array to contain all possible departure time
-		TrainArrivalTime = new string[size];
+			check_hours = stoi(TrainArrivalTime[i].substr(0, pos));
+			check_minutes = stoi(TrainArrivalTime[i].substr(pos + 1));
 
-		//Calculate all possible departure time
-		int count = 0;
-		while (count != size) {
-			if (hour == 24)
+			if (CurrentHours > check_hours)
 			{
-				hour = 0;
+				size--;
+				continue;
 			}
+			else if (CurrentHours == check_hours)
+			{
+				if (CurrentMins >= check_minutes)
+				{
+					size--;
+					continue;
+				}
+
+			}
+
+			cout << arrayindex2 + 1 << ") " << setw(2) << setfill('0') << to_string(check_hours) << ":" << setw(2) << setfill('0') << to_string(check_minutes) << endl;
+			TodayTrainArrivalTime[arrayindex2] = TrainArrivalTime[i];
+			arrayindex2++;
 			
-			cout << count + 1 << ") " << setw(2) << setfill('0') << to_string(hour) << ":" << setw(2) << setfill('0') << to_string(minutes) << endl;
-			TrainArrivalTime[count] = to_string(hour) + ":" + to_string(minutes);
-			count++;
-			minutes = minutes + 30;
-			if (minutes > 59)
-			{
-				minutes = minutes - 60;
-				hour = hour + 1;
-			}
 		}
-
 	}
+
 
 	while (true)
 	{
@@ -352,7 +468,14 @@ string listAllPossibleDepartureTime(int route, int departure, int todaydate)
 		}
 		else
 		{
-			return TrainArrivalTime[DepartureTimeIndex - 1];
+			if (todaydate == 0)
+			{
+				return TrainArrivalTime[DepartureTimeIndex - 1];
+			}
+			else if (todaydate == 1)
+			{
+				return TodayTrainArrivalTime[DepartureTimeIndex - 1];
+			}
 		}
 	}
 
@@ -387,7 +510,7 @@ void displayDetailsBetweenTwoSelectedCities(int route, int departure, int arriva
 
 		cout << "Arrival Station: " << stationCurrent->StationName << endl;
 		cout << "Total Distance: " << totaldistance << " KM" << endl;
-		cout << "Total Expenses: RM " << totalexpenses << endl;
+		cout << "Total Expenses per ticket: RM " << totalexpenses << endl;
 		cout << "Total Time: " << totaltime << " Minutes" << endl << endl;
 
 		cout << "Departure Time: " << Year << "/" << setw(2) << setfill('0') << Month << "/" << setw(2) << setfill('0') <<
@@ -457,7 +580,7 @@ void displayDetailsBetweenTwoSelectedCities(int route, int departure, int arriva
 
 
 //Customer Purchase Ticket
-void purchaseticket()
+void purchaseticket(int customerId)
 {
 	int route;
 	//Select Route
@@ -641,11 +764,12 @@ void purchaseticket()
 	
 }
 
+
 int main()
 {
 	stationHead = stationTail = NULL;
 
-	int role = 0;
+	
 	Station* StationNode1 = createStationNode(1, "Titiwangsa", "", "PWTC", 0, 0, 0, 4, 0.40, 3, "NearbySeeingSpot1");
 	Station* StationNode2 = createStationNode(2, "PWTC", "Titiwangsa", "Sultan Ismail", 4, 0.40, 3, 8, 0.80, 7, "NearbySeeingSpot2");
 	Station* StationNode3 = createStationNode(3, "Sultan Ismail", "PWTC", "Majlis Jamek", 8, 0.80, 7, 8, 0.80, 7, "NearbySeeingSpot3");
@@ -655,34 +779,144 @@ int main()
 	Station* StationNode7 = createStationNode(7, "Pudu", "Hang Tuah", "Chan Sow Lin", 5, 0.50, 4, 5, 0.50, 4, "NearbySeeingSpot7");
 	Station* StationNode8 = createStationNode(8, "Chan Sow Lin", "Pudu", "", 5, 0.50, 4, 0, 0, 0, "NearbySeeingSpot8");
 
-	while (role == 0)
+	while (true)
 	{
-		cout << "1) Passenger" << endl;
-		cout << "2) Admin" << endl;
-		cout << "Select your role: ";
-		cin >> role;
 
-		if (role == 1)
+		int decision;
+		system("CLS");
+		cout << "1) Login to existing account" << endl;
+		cout << "2) Create a new account as passenger" << endl;
+		cout << "3) Exit system" << endl;
+		cout << "Select your decision: " << endl;
+		cin >> decision;
+
+
+		if (decision == 1)
 		{
-			insertToTheFrontList(StationNode8);
-			insertToTheFrontList(StationNode7);
-			insertToTheFrontList(StationNode6);
-			insertToTheFrontList(StationNode5);
-			insertToTheFrontList(StationNode4);
-			insertToTheFrontList(StationNode3);
-			insertToTheFrontList(StationNode2);
-			insertToTheFrontList(StationNode1);
+			while (true)
+			{
+				int role;
+				cout << "1) Passenger" << endl;
+				cout << "2) Admin" << endl;
+				cout << "Select your role: ";
+				cin >> role;
 
-			purchaseticket();
+				//Passenger
+				if (role == 1)
+				{
+					string customerAccount;
+					string customerPassword;
+					cout << "Please Enter your IC or Passport Number" << endl;
+					cin >> customerAccount;
+					cout << "Please Enter your Password" << endl;
+					cin >> customerPassword;
+
+					int customerId = checkCustomerAccount(customerAccount, customerPassword);
+
+					if (customerId != 0)
+					{
+						insertStationNodeToTheFrontList(StationNode8);
+						insertStationNodeToTheFrontList(StationNode7);
+						insertStationNodeToTheFrontList(StationNode6);
+						insertStationNodeToTheFrontList(StationNode5);
+						insertStationNodeToTheFrontList(StationNode4);
+						insertStationNodeToTheFrontList(StationNode3);
+						insertStationNodeToTheFrontList(StationNode2);
+						insertStationNodeToTheFrontList(StationNode1);
+
+						int customerDecision;
+						while (true)
+						{
+							cout << "1) Purchase Ticket" << endl;
+							cout << "2) Search subway station details" << endl;
+							cout << "3) View transaction history" << endl;
+							cout << 
+							cout << "2) Exit to Main Menu" << endl;
+							cin >> customerDecision;
+							if (customerDecision == 1)
+							{
+								purchaseticket(customerId);
+							}
+							else
+							{
+								cout << "Thank you, have a nice day. " << endl;
+								system("pause");
+								break;
+							}
+
+						}
+					}
+
+					break;
+				}
+
+				//Admin
+				else if (role == 2)
+				{
+
+					cout << "Admin";
+				}
+			}
+
 		}
-
-			
-
-	
-		else if (role == 2)
+		else if (decision == 2)
 		{
-			//Admin
-			cout << "Admin";
+			while (true)
+			{
+				system("CLS");
+				string customerName;
+				string customerIcOrPassport;
+				string customerPassword;
+				cout << "Please Enter your Full Name: ";
+				cin >> customerName;
+				cout << "Please Enter your IC or Passport Number: ";
+				cin >> customerIcOrPassport;
+				cout << "Please Create a password: ";
+				cin >> customerPassword;
+
+				int status = createCustomerAccount(customerName, customerIcOrPassport, customerPassword);
+				if (status == 0)
+				{
+					
+					string ContinueCreate;
+					cout << "This account is already existed. " << endl;
+					cout << "Continue Create account? " << endl;
+					cout << "1) Yes    2) No";
+					cin >> ContinueCreate;
+
+					if (ContinueCreate == "1")
+					{
+						continue;
+					}
+					else if (ContinueCreate == "2")
+					{
+						break;
+					}
+					else
+					{
+						cout << "Invalid Input. Returning to Main Menu." << endl;
+					}
+				}
+				else
+				{
+					cout << "Account created successfully." << endl;
+					system("pause");
+					break;
+
+				}
+			}
+
+		}
+		else if (decision == 3)
+		{
+			break;
+		}
+		else
+		{
+			cin.clear(); //remove the input operation
+			cin.ignore(numeric_limits<streamsize>::max(), '\n'); //remove content
+			cout << "Invalid Format" << endl;
+			system("pause");
 		}
 
 	}
